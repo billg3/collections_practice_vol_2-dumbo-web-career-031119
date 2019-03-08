20ed2900159d41ad0ea9297a0e5aeaa664081c81
Bill G <gbilly3@gmail.com>
2019-03-08 18:15:08 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,16 @@
 # your code goes here
+##describe '#begins_with_r' do
+    # Question 1
+
+   # it 'Return true if every element of the tools array starts with an "r" and false otherwise.' do
+     # expect(begins_with_r(["ruby", "rspec", "rails"])).to eq(true)
+  #  end
+
+  #  it "should return false if there's an element that does not begin with r" do
+     # expect(begins_with_r(["ruby", "rspec", "sails"])).to eq(false)
+  #  end
+#
+#  end
+
+
+de begins_with_r()
